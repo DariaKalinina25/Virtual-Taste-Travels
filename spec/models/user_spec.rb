89a2608ea7b2fcc976bd4e0dad18c72d7e5f6005
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-05-14 00:10:58 +0400

    Add a test for the model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'devise modules' do
+    it { should respond_to(:email) }
+    it { should respond_to(:encrypted_password) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_created_at) }
+    it { should respond_to(:reset_password_token) }
+    it { should respond_to(:reset_password_sent_at) }
+    it { should respond_to(:remember_created_at) }
+  end
 end
