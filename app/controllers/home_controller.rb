b6aae2da8d3d278ce3d1858e9462c9ec3d418d87
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-05-18 01:44:03 +0400

    Refactor using "rubocop"

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
+# HomeController handles the homepage of the application.
 class HomeController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users, skip: %i[confirmations passwords]
   root 'home#index'
-  get 'up' => 'rails/health#show', as: :rails_health_check 
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
