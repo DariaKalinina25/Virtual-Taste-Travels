f3223359e5a83777d22566f264f756ce89782d6f
DariaKalinina25 <meuw.meuw20@gmail.com>
2024-05-17 15:44:28 +0400

    Refactor using "rubocop"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, skip: [:confirmations, :passwords]
+  devise_for :users, skip: %i[confirmations passwords]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
